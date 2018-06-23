d5abee4a60651df5ff6156c8ce2dcdc51d401cff
cklll <a418316118abc@gmail.com>
2018-06-23 22:56:26 +0800

    initial react, express & nodejs setup

---
+++ client/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <p>header</p>
+        </header>
+        <div className="MainContainer">
+          <p>main23</p>
+        </div>
+        <footer>
+          <p>footer</p>
+          </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
